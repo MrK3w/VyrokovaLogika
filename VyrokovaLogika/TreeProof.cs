d63ce6ecfebb45bf0593f81d0d1defb57aecde07
pietsjak <pietsjak@W49CP8D3>
2023-04-12 22:07:10 +0200

    update

--- VyrokovaLogika/TreeProof.cs
+++ VyrokovaLogika/TreeProof.cs
@@ -15,41 +15,97 @@ namespace VyrokovaLogika
         {
         }
 
-        public void ProcessTree(Tree tree, TruthTree truthTree = null)
+        public List<Tuple<TruthTree,TruthTree>> ProcessTree(Tree tree, int truthValue = 0)
         {
-            List<TruthTree> TempTrees = new List<TruthTree>();
-            if (tree.IsRoot)
+            List<TruthTree> listOftrees = new List<TruthTree>();
+            if(tree.IsLeaf)
             {
-                truthTree = new TruthTree(0);
+                TruthTree tTree = new TruthTree(truthValue);
+                List<Tuple<TruthTree, TruthTree>> treeees = new List<Tuple<TruthTree, TruthTree>>();
+
+                // Add a tuple with tTree as the first element and null as the second element to the list
+                treeees.Add(new Tuple<TruthTree, TruthTree>(tTree, null));
+
+                return treeees;
             }
-            var sideValues = Rule.GetValuesOfBothSides(truthTree.Item, tree.Item.mOperator);
-            //it's just a copy of previous tree must be redone
+            List<Tuple<int, int>> list = new List<Tuple<int, int>>();
+            var sideValues = Rule.GetValuesOfBothSides(truthValue, tree.Item.mOperator);
             foreach (var sideValue in sideValues)
             {
-                //TruthTree newTree = truthTree.Clone();
-                if(tree.childNodeLeft != null)
+                int leftSide = 0;
+                int rightSide = 0;
+                if (tree.childNodeLeft != null)
                 {
-                    newTree.AddChild(sideValue.Item1, "left");   
+                    leftSide = sideValue.Item1;  
                 }
                 if (tree.childNodeRight != null)
                 {
-                    newTree.AddChild(sideValue.Item2, "right");
+                    rightSide = sideValue.Item2;
                 }
-                TempTrees.Add(newTree);
+                list.Add(new Tuple<int,int>(leftSide, rightSide));
             }
-            
-            foreach (var newTree in TempTrees)
+
+            foreach (var newTree in list)
             {
+                List<TruthTree> tempTreeListLeft = new List<TruthTree> ();
+                List<TruthTree> tempTreeListRight = new List<TruthTree>();
+                TruthTree tTree = new TruthTree(truthValue);
                 if (tree.childNodeLeft != null)
                 {
-                    ProcessTree(tree.childNodeLeft, newTree.ChildNodeLeft);
+                    var xd = ProcessTree(tree.childNodeLeft, newTree.Item1);
+                    foreach (var tyhlestromy in xd)
+                    {
+                        tTree = new TruthTree(truthValue);
+                        if (tyhlestromy.Item1 != null)
+                        {
+                            tTree.AddChild(tyhlestromy.Item1.Item, "left");
+                        }
+                        if (tyhlestromy.Item2 != null)
+                        {
+                            tTree.AddChild(tyhlestromy.Item2.Item, "right");
+                        }
+
+                        tempTreeListLeft.Add(tTree);
+                    }
+
+
                 }
                 if(tree.childNodeRight != null)
                 {
-                    ProcessTree(tree.childNodeRight, newTree.ChildNodeRight);
+                    var xd = ProcessTree(tree.childNodeRight, newTree.Item2);
+                    foreach (var tyhlestromy in xd)
+                    {
+                        tTree = new TruthTree(truthValue);
+                        if (tyhlestromy.Item1 != null)
+                        {
+                            tTree.AddChild(tyhlestromy.Item1.Item, "left");
+                        }
+                        if (tyhlestromy.Item2 != null)
+                        {
+                            tTree.AddChild(tyhlestromy.Item2.Item, "right");
+                        }
+
+                        tempTreeListRight.Add(tTree);
+                    }
                 }
-               
+
+                List<Tuple<TruthTree, TruthTree>> treePairs = new List<Tuple<TruthTree, TruthTree>>();
+
+                // Iterate through the list of trees
+                for (int i = 0; i < tempTreeListLeft.Count; i++)
+                {
+                    for (int j = 0; j < tempTreeListRight.Count; j++)
+                    {
+                        // Create a tuple with the current pair of trees
+                        Tuple<TruthTree, TruthTree> treePair = new Tuple<TruthTree, TruthTree>(tempTreeListLeft[i], tempTreeListRight[j]);
+
+                        // Add the tuple to the list of tree pairs
+                        treePairs.Add(treePair);
+                    }
+                }
+                return treePairs;
             }
+            return null;
         }
 
         public bool isTautology()
